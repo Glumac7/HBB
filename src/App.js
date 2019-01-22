211b20a9d6657bf82a7e398fad3d648028207c84
Glumac7 <necaglumicic@gmail.com>
2019-01-22 20:45:32 +0100

    Started the development of the app. Going good so far!

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Display/Navbar';
+import Home from './components/Display/Home';
 
 class App extends Component {
+
+  
   render() {
     return (
+      <BrowserRouter>
       <div className="App">
+      
+        <Navbar></Navbar>
+      
+        <Switch>
+          <Route path="/" component={Home}/>
+        </Switch>
+      
         
       </div>
+      </BrowserRouter>
     );
   }
 }
