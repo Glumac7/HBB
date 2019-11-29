a3e9e359691d296087e082b71ea4cfb94e080ed4
Nemanja Glumicic <necaglumicic@gmail.com>
2019-11-29 18:26:39 +0100

    Some mobile style stuff.

--- src/components/Display/Home.js
+++ src/components/Display/Home.js
@@ -39,12 +39,12 @@ export default class Home extends React.Component {
   
             <div id="about-text" className="col-md-6">
               <h1>About</h1>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus possimus odit veritatis magnam sequi qui in, cupiditate quam cum obcaecati inventore ea reprehenderit nihil saepe, omnis repudiandae odio eveniet id!</p>
+              <p>Booker is an app that allows you to create an account in which you can record books that you <b>have</b> or that you want to <b>buy</b>.</p>
             </div>
   
             <div id="about-photos" className="col-md-6">
-              <img src={  require('../../images/about-1.jpg')  } style={{  maxWidth: "50%"  }}alt=""/>
-              <img src={  require('../../images/about-2.jpg')  } style={{  maxWidth: "50%"  }}alt=""/>
+              <img src='https://images.unsplash.com/photo-1508107506371-193d6e71444c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80' style={{  maxWidth: "50%"  }}alt=""/>
+              <img src='https://images.unsplash.com/photo-1495640452828-3df6795cf69b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=80' style={{  maxWidth: "50%"  }}alt=""/>
             </div>
   
           </div>
